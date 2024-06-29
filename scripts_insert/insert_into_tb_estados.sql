71b32be1c026a4d1b83d25ac42f34bf1d0c1406b
Vinicius Molon <molon.vinicius@gmail.com>
2024-06-29 10:28:11 -0300

    Update insert_into_tb_estados.sql

--- scripts_insert/insert_into_tb_estados.sql
+++ scripts_insert/insert_into_tb_estados.sql
@@ -1,3 +1,4 @@
+/* Adicionando todos os estados brasileiros */
 declare @Nome_Pais varchar(100)
 set @Nome_Pais = 'Brasil'
 
@@ -6,7 +7,7 @@ set @ID_Pais = (select ID_Pais from tb_paises with(nolock) where Nome_Pais = @No
 
 if @ID_Pais is null
 begin
-  select 'PaÌs informado n„o encontrado.'
+  select 'Pa√≠s informado n√£o encontrado.'
   return
 end
 
@@ -16,29 +17,29 @@ insert into tb_estados (Nome_Estado, ID_Pais, Sigla_Estado)
 
 values('Acre',@ID_Pais,'AC')
      ,('Alagoas',@ID_Pais,'AL')
-     ,('Amap·',@ID_Pais,'AP')
+     ,('Amap√°',@ID_Pais,'AP')
      ,('Amazonas',@ID_Pais,'AM')
      ,('Bahia',@ID_Pais,'BA')
-     ,('Cear·',@ID_Pais,'CE')
+     ,('Cear√°',@ID_Pais,'CE')
      ,('Distrito Federal',@ID_Pais,'DF')
-     ,('EspÌrito Santo',@ID_Pais,'ES')
-     ,('Goi·s',@ID_Pais,'GO')
-     ,('Maranh„o',@ID_Pais,'MA')
+     ,('Esp√≠rito Santo',@ID_Pais,'ES')
+     ,('Goi√°s',@ID_Pais,'GO')
+     ,('Maranh√£o',@ID_Pais,'MA')
      ,('Mato Grosso',@ID_Pais,'MT')
      ,('Mato Grosso do Sul',@ID_Pais,'MS')
      ,('Minas Gerais',@ID_Pais,'MG')
-     ,('Par·',@ID_Pais,'PA')
-     ,('ParaÌba',@ID_Pais,'PB')
-     ,('Paran·',@ID_Pais,'PR')
+     ,('Par√°',@ID_Pais,'PA')
+     ,('Para√≠ba',@ID_Pais,'PB')
+     ,('Paran√°',@ID_Pais,'PR')
      ,('Pernambuco',@ID_Pais,'PE')
-     ,('PiauÌ',@ID_Pais,'PI')
+     ,('Piau√≠',@ID_Pais,'PI')
      ,('Rio de Janeiro',@ID_Pais,'RJ')
      ,('Rio Grande do Norte',@ID_Pais,'RN')
      ,('Rio Grande do Sul',@ID_Pais,'RS')
-     ,('RondÙnia',@ID_Pais,'RO')
+     ,('Rond√¥nia',@ID_Pais,'RO')
      ,('Roraima',@ID_Pais,'RR')
      ,('Santa Catarina',@ID_Pais,'SC')
-     ,('S„o Paulo',@ID_Pais,'SP')
+     ,('S√£o Paulo',@ID_Pais,'SP')
      ,('Sergipe',@ID_Pais,'SE')
      ,('Tocantins',@ID_Pais,'TO')
 end
