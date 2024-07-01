699aca5084ddb76482451a5277b46c7e1e1842c4
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-01 10:42:21 -0300

    Update alter_table_tb_cidades.sql

--- alter_table/alter_table_tb_cidades.sql
+++ alter_table/alter_table_tb_cidades.sql
@@ -1,9 +1,3 @@
-/* No caso abaixo será necessário a criação de duas Constraints por conta da relação com a tabela 'tb_estados' */
-/* A priori a tabela 'tb_estados' só terá relação com cidades do Brasil, porém, existem algumas cidades de mesmo nome em estados diferentes */
-/* Há também cidades com mesmo nome em países diferentes também, por isso a criação das duas restrições abaixo */
-
-ALTER TABLE tb_cidades
-ADD UNIQUE (Nome_Cidade, ID_Estado)
 
 ALTER TABLE tb_cidades
 ADD UNIQUE (Nome_Cidade, ID_Pais)
