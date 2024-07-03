bce6292f4a7f1716a19775bbb0424d8cab2da78f
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-03 02:08:41 -0300

    Create tb_selecoes.sql

---
+++ scripts_create_table/tb_selecoes.sql
@@ -0,0 +1,12 @@
+CREATE TABLE tb_selecoes
+(ID_Selecao INT IDENTITY(1,1) PRIMARY KEY NOT NULL
+,Nome_Selecao VARCHAR(128) NOT NULL
+,ID_Confederacao INT NOT NULL
+,Ativo VARCHAR(1) NULL)
+
+ALTER TABLE tb_selecoes WITH CHECK ADD FOREIGN KEY(ID_Confederacao)
+REFERENCES tb_confederacoes (ID_Confederacao)
+GO
+
+ALTER TABLE tb_selecoes ADD CONSTRAINT df_selecoes_ativas DEFAULT ('S') FOR Ativo
+GO
