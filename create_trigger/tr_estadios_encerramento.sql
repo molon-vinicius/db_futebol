03e08cf2d1cc3eeefc9ada7123678a74bc861d6e
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-03 02:41:45 -0300

    Create tr_estadios_encerramento.sql

---
+++ create_trigger/tr_estadios_encerramento.sql
@@ -0,0 +1,25 @@
+create trigger tr_estadios_encerramento
+            on tb_estadios
+for insert, update 
+ 
+as
+
+declare @ativo varchar(1)
+declare @encerramento varchar(10)
+
+    select @ativo        = Ativo
+         , @encerramento = Encerramento
+      from inserted 
+
+  if @ativo = 'N' and @encerramento is null
+  begin
+    raiserror ('É necessário informar uma data de encerramento para estádios inativos.', 11, 127)
+	  rollback transaction
+  end
+
+  if @ativo = 'S' and @encerramento is not null
+  begin
+    raiserror ('Não é possível inserir um estádio ativo com data de encerramento.', 11, 127)
+	  rollback transaction
+  end
+ 
