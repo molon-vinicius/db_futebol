8ac0a3d800d0859e2e15e8c62ecaba18e5949bc7
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-03 02:05:42 -0300

    Update tb_campeonatos_edicoes_times_part.sql

--- scripts_create_table/tb_campeonatos_edicoes_times_part.sql
+++ scripts_create_table/tb_campeonatos_edicoes_times_part.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tb_campeonatos_edicoes_times_part
 (ID_Campeonato_Edicao INT IDENTITY(1,1) NOT NULL
-,ID_Time NUMERIC(15) NULL
+,ID_Time NUMERIC(15) NOT NULL
 ,Obs VARCHAR(40) NULL)
 
 ALTER TABLE tb_campeonatos_edicoes_times_part WITH CHECK ADD FOREIGN KEY(ID_Time)
