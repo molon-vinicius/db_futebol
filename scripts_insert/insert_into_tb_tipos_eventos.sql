d870a097ef68aa17aec65643c4dce851a297ef8b
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-03 15:21:47 -0300

    Create insert_into_tb_tipos_eventos.sql

---
+++ scripts_insert/insert_into_tb_tipos_eventos.sql
@@ -0,0 +1,9 @@
+insert into tb_tipos_eventos
+           (Descricao)
+    values ('Gol')
+         , ('Cartão Amarelo')
+         , ('Cartão Vermelho')
+         , ('Gol (P)')
+         , ('Pênalti (X)')
+         , ('Gol Anulado')
+         , ('Gol Contra')
