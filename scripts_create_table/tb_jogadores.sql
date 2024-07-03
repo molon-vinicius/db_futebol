468aab21e34abdd1bbc2654b257b98217c59b99d
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-03 19:39:20 -0300

    Create tb_jogadores.sql

---
+++ scripts_create_table/tb_jogadores.sql
@@ -0,0 +1,22 @@
+CREATE TABLE tb_jogadores
+(ID_Jogador INT IDENTITY(1,1) PRIMARY KEY NOT NULL
+,Pe_Preferencial VARCHAR(1) NULL
+,ID_Pessoa INT NOT NULL
+,Ambidestro VARCHAR(1) NULL
+,Lado_Oposto VARCHAR(1) NULL
+,Dois_Lados VARCHAR(1) NULL) 
+GO
+
+ALTER TABLE tb_jogadores WITH CHECK ADD FOREIGN KEY(ID_Pessoa)
+REFERENCES tb_pessoas (ID_Pessoa)
+GO
+
+ALTER TABLE tb_jogadores ADD CONSTRAINT df_ambidestro DEFAULT ('N') FOR Ambidestro
+GO
+
+ALTER TABLE tb_jogadores ADD CONSTRAINT df_lado_oposto DEFAULT ('N') FOR Lado_Oposto
+GO
+
+ALTER TABLE tb_jogadores ADD CONSTRAINT df_dois_lados DEFAULT ('N') FOR Dois_Lados
+GO
+
