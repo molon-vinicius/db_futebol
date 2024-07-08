f0327ed8b9635da1c0473319cd3e02b89d2ddb7f
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-07 23:48:09 -0300

    Update tb_times_elencos.sql

--- scripts_create_table/tb_times_elencos.sql
+++ scripts_create_table/tb_times_elencos.sql
@@ -1,14 +1,14 @@
 CREATE TABLE tb_times_elencos 
 (ID_Time_Elenco NUMERIC(15) IDENTITY(1,1) PRIMARY KEY NOT NULL
-,ID_Time NUMERIC(15) NULL
-,Temporada VARCHAR(9) NULL
-,ID_Jogador INT NULL
+,ID_Time NUMERIC(15) NOT NULL
+,Temporada VARCHAR(9) NOT NULL
+,ID_Jogador INT NOT NULL
 ,Camisa VARCHAR(2) NULL
-,Data_Entrada DATE NULL
-,Data_Saida DATE NULL
+,Data_Entrada DATE NOT NULL
+,Data_Saida DATE NOT NULL
 ,Capitao VARCHAR(1) NULL
 ,Nome_Camisa VARCHAR(20) NULL
-,Posicoes VARCHAR(20) NULL)
+,Posicoes VARCHAR(20) NOT NULL)
 
 ALTER TABLE tb_times_elencos WITH CHECK ADD FOREIGN KEY(ID_Jogador)
 REFERENCES tb_jogadores (ID_Jogador)
