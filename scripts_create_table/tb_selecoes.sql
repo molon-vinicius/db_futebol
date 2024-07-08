630e76a70025f7d1105bd1e46ad9243474996a73
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-07 23:36:10 -0300

    Update tb_selecoes.sql

--- scripts_create_table/tb_selecoes.sql
+++ scripts_create_table/tb_selecoes.sql
@@ -2,7 +2,9 @@ CREATE TABLE tb_selecoes
 (ID_Selecao INT IDENTITY(1,1) PRIMARY KEY NOT NULL
 ,Nome_Selecao VARCHAR(128) NOT NULL
 ,ID_Confederacao INT NOT NULL
-,Ativo VARCHAR(1) NULL)
+,Ativo VARCHAR(1) NULL
+,Data_Fundacao VARCHAR(10)
+,Data_Encerramento VARCHAR(10))
 
 ALTER TABLE tb_selecoes WITH CHECK ADD FOREIGN KEY(ID_Confederacao)
 REFERENCES tb_confederacoes (ID_Confederacao)
