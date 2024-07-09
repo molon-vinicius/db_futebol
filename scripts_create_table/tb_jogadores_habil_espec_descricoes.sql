8214cc11b9290dd430e13766d5454bcec0be63d4
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-08 23:08:50 -0300

    Create tb_jogadores_habil_espec_descricoes.sql

---
+++ scripts_create_table/tb_jogadores_habil_espec_descricoes.sql
@@ -0,0 +1,12 @@
+create table tb_jogadores_habil_espec_descricoes
+(Desc_Dribbling varchar(320)
+,Desc_Tactical_Dribble varchar(320)
+,Desc_Positioning varchar(320)
+,Desc_Playmaking varchar(320)
+,Desc_Scoring varchar(320)
+,Desc_Post_Player varchar(320)
+,Desc_Lines varchar(320)
+,Desc_Middle_Shooting varchar(320)
+,Desc_Side varchar(320)
+,Desc_Center varchar(320)
+,Desc_Curling_Shot varchar(320))
