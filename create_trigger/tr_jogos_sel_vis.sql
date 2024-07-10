a41b5963e9480591637907ede82855e459b18970
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-10 08:33:09 -0300

    Create tr_jogos_sel_vis.sql

---
+++ create_trigger/tr_jogos_sel_vis.sql
@@ -0,0 +1,22 @@
+create trigger tr_jogos_sel_vis 
+            on tb_jogos_selecoes_visitantes
+for insert, update 
+ 
+as
+
+begin
+
+declare @id int = (select ID_jogo_selecao from inserted)
+declare @qtd_jogadores int 
+
+	   select @qtd_jogadores = count(a.ID_jogador)
+	     from tb_jogos_selecoes_visitantes a with(nolock)
+		  where a.ID_jogo_selecao = @id
+  
+  if @qtd_jogadores > 11
+  begin
+     raiserror ('Quantidade de jogadores titulares já atingida.', 11, 127)
+	   rollback transaction
+  end
+
+end
