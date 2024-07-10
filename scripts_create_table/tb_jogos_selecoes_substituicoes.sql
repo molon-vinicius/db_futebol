7309fbcb0b0e8c526d0137e7fa345bdd0ce5fc43
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-10 09:02:44 -0300

    Create tb_jogos_selecoes_substituicoes.sql

---
+++ scripts_create_table/tb_jogos_selecoes_substituicoes.sql
@@ -0,0 +1,23 @@
+CREATE TABLE tb_jogos_selecoes_substituicoes
+(ID_Jogo_Selecao_Substituicao NUMERIC(15) IDENTITY(1,1) PRIMARY KEY NOT NULL
+,ID_Jogo_Selecao NUMERIC(15) NOT NULL
+,ID_Selecao INT NOT NULL
+,Minuto TINYINT NULL
+,ID_Jogador_Saida INT NOT NULL
+,ID_Jogador_Entrada INT NOT NULL)
+
+ALTER TABLE tb_jogos_selecoes_substituicoes WITH CHECK ADD FOREIGN KEY(ID_Jogo_Selecao)
+REFERENCES tb_jogos_selecoes (ID_Jogo_Selecao)
+GO
+
+ALTER TABLE tb_jogos_selecoes_substituicoes WITH CHECK ADD FOREIGN KEY(ID_Jogador_Saida)
+REFERENCES tb_jogadores (ID_Jogador)
+GO
+
+ALTER TABLE tb_jogos_selecoes_substituicoes WITH CHECK ADD FOREIGN KEY(ID_Jogador_Entrada)
+REFERENCES tb_jogadores (ID_Jogador)
+GO
+
+ALTER TABLE tb_jogos_selecoes_substituicoes WITH CHECK ADD FOREIGN KEY(ID_selecao)
+REFERENCES tb_selecoes (ID_Selecao)
+GO
