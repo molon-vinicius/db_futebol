ece43c38f0e644b168471a95acee12c722d06248
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-13 02:28:14 -0300

    Create tb_jogos_times_visitantes.sql

---
+++ scripts_create_table/tb_jogos_times_visitantes.sql
@@ -0,0 +1,17 @@
+CREATE TABLE tb_jogos_times_visitantes
+(ID_Jogo_Time_Visitante NUMERIC(15) IDENTITY(1,1) PRIMARY KEY NOT NULL
+,ID_Jogo_Time NUMERIC(15) NOT NULL
+,ID_Time NUMERIC(15) NOT NULL
+,ID_Jogador INT NOT NULL)
+
+ALTER TABLE tb_jogos_times_visitantes WITH CHECK ADD FOREIGN KEY(ID_Jogo_Time)
+REFERENCES tb_jogos_times (ID_Jogo_Time)
+GO
+
+ALTER TABLE tb_jogos_times_visitantes WITH CHECK ADD FOREIGN KEY(ID_Jogador)
+REFERENCES tb_jogadores (ID_Jogador)
+GO
+
+ALTER TABLE tb_jogos_times_visitantes WITH CHECK ADD FOREIGN KEY(ID_Time)
+REFERENCES tb_times (ID_Time)
+GO
