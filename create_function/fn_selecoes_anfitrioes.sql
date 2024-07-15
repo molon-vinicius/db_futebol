10c1d5dbea906ca1fcef6665f63f6f3ea921f4b0
Vinicius Molon <molon.vinicius@gmail.com>
2024-07-15 03:25:20 -0300

    Update fn_selecoes_anfitrioes.sql

--- create_function/fn_selecoes_anfitrioes.sql
+++ create_function/fn_selecoes_anfitrioes.sql
@@ -3,6 +3,7 @@ create function fn_selecoes_anfitrioes
 returns @elenco table 
 (camisa int
 ,capitao varchar(1)
+,id_jogador int
 ,nome_reduzido varchar(60)
 ,id_posicao int
 ,posicoes varchar(60))
@@ -19,25 +20,25 @@ as
 --,posicoes varchar(60))
 
 begin
-insert into @elenco (camisa, capitao, nome_reduzido, id_posicao, posicoes)
-    select a.camisa
-         , a.capitao
-         , case when a.capitao = 'P'
-                then concat(c.Nome_Reduzido, ' (C)')
-		            else c.Nome_Reduzido
-           end                 as Nome_Reduzido
-         , d.id_posicao
-		     , a.posicoes 
-	    from tb_jogos_selecoes            x with(nolock)
-	  	join tb_selecoes_elencos          a with(nolock)on a.ID_selecao = x.ID_selecao_anfitriao
-		  join tb_jogos_selecoes_anfitrioes b with(nolock)on b.ID_selecao = a.ID_selecao
-                                                     and b.ID_jogador = a.ID_jogador
-			                        										   and b.ID_jogo_selecao = x.ID_jogo_selecao
-        join vw_jogadores               c with(nolock)on c.ID_jogador = a.ID_jogador
-        join tb_posicoes                d with(nolock)on d.Sigla_Posicao = case when substring(a.posicoes,0,2) in ('R','L')
-                                                                                then substring(a.posicoes,2,2)
-                                                                                else substring(a.posicoes,0,3)
-                                                                           end 
+insert into @elenco (camisa, capitao, id_jogador, nome_reduzido, id_posicao, posicoes)
+      select a.camisa
+           , a.capitao
+           , a.id_jogador
+           , case when a.capitao = 'P'
+                  then concat(c.Nome_Reduzido, ' (C)')
+                  else c.Nome_Reduzido
+             end                 as Nome_Reduzido
+           , d.id_posicao
+           , a.posicoes 
+        from tb_jogos_selecoes            x with(nolock)
+        join tb_selecoes_elencos          a with(nolock)on a.ID_selecao = x.ID_selecao_anfitriao
+	join tb_jogos_selecoes_anfitrioes b with(nolock)on b.ID_selecao = a.ID_selecao
+                                                       and b.ID_jogador = a.ID_jogador			                        										   and b.ID_jogo_selecao = x.ID_jogo_selecao
+        join vw_jogadores                 c with(nolock)on c.ID_jogador = a.ID_jogador
+        join tb_posicoes                  d with(nolock)on d.Sigla_Posicao = case when substring(a.posicoes,0,2) in ('R','L')
+                                                                                  then substring(a.posicoes,2,2)
+                                                                                  else substring(a.posicoes,0,3)
+                                                                             end 
        where b.ID_jogo_selecao = @id_jogo_selecao
 
 if not exists ( 
@@ -47,7 +48,7 @@ if not exists (
 begin
    update @elenco
       set nome_reduzido = concat(nome_reduzido, ' (C)')
-  	where capitao = 'S'
+    where capitao = 'S'
 end
    return
 end
