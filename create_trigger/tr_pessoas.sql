0352d79f3416309e76045fc3cd1b9b49f840d41a
Vinicius Molon <molon.vinicius@gmail.com>
2024-08-08 13:23:16 -0300

    Update tr_pessoas.sql
    Adicionando validações de formato de data e ordem cronológica.

--- create_trigger/tr_pessoas.sql
+++ create_trigger/tr_pessoas.sql
@@ -8,10 +8,14 @@ begin
 declare @id_cid_nasc  int
 declare @id_pais_nasc int
 declare @id_dupla_cid int
+declare @dt_nasc      varchar(10)
+declare @dt_obt       varchar(10)
 declare @retorno      varchar(150)
 
         select @id_cid_nasc  = ID_Cidade_Nascimento 
              , @id_dupla_cid = Dupla_Cidadania
+             , @dt_nasc      = Data_Nascimento
+             , @dt_obt       = Data_Obito
           from inserted
 
         select @id_pais_nasc = b.ID_Pais
@@ -25,5 +29,26 @@ declare @retorno      varchar(150)
        rollback transaction
     end
 	
+    if len(@dt_nasc) < 10
+    or try_convert(date, @dt_nasc) is null
+    begin
+       raiserror ('Formato da data de nascimento é inválido.', 11, 127)
+       rollback transaction
+    end
+
+    if len(@dt_obt) < 10
+    or try_convert(date, @dt_obt) is null
+    begin
+       raiserror ('Formato da data de óbito é inválido.', 11, 127)
+       rollback transaction
+    end
+
+    if convert(date, @dt_nasc) > convert(date, @dt_obt)
+    or convert(date, @dt_nasc) = convert(date, @dt_obt)
+    begin
+       raiserror ('A ordem cronológica das datas de nascimento e de óbito estão incorretas.', 11, 127)
+       rollback transaction
+    end
+
 end
 
