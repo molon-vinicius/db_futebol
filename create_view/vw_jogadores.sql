11e4e4f636e7499205073be5083eb4ba841297ae
Vinicius Molon <molon.vinicius@gmail.com>
2024-09-06 18:53:58 -0300

    Update vw_jogadores.sql
    informações da tabela 'tb_jogadores' adicionadas

--- create_view/vw_jogadores.sql
+++ create_view/vw_jogadores.sql
@@ -1,9 +1,20 @@
-CREATE view vw_jogadores 
+create view vw_jogadores 
   
 as  
   select a.ID_Jogador
        , b.Nome_Reduzido  
-       , dbo.fn_pessoas_nacionalidade(b.Nome_Completo) as Nacionalidade  
+       , dbo.fn_pessoas_nacionalidade(b.Nome_Completo) as Nacionalidade
+       , case when Pe_Preferencial = 'R'
+              then 'Destro'
+              when Pe_Preferencial = 'L'
+              then 'Canhoto'
+              when Ambidestro = 'S'
+              then 'Ambidestro'
+              else 'Sem informação'
+         end                                           as Pe_Preferencial       
+       , a.Dois_Lados
+       , a.Lado_Oposto       	   
     from tb_jogadores    a with(nolock)  
     join tb_pessoas      b with(nolock)on b.ID_Pessoa = a.ID_Pessoa  
+
 
