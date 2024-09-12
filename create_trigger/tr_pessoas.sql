dc0ddd05d66721a89fc9ce0ca43ca7dbd2ca9a3f
Vinicius Molon <molon.vinicius@gmail.com>
2024-09-12 00:23:26 -0300

    Update tr_pessoas.sql

--- create_trigger/tr_pessoas.sql
+++ create_trigger/tr_pessoas.sql
@@ -1,4 +1,4 @@
-CREATE trigger tr_pessoas
+create trigger tr_pessoas
             on tb_pessoas
 for insert, update 
   
@@ -20,7 +20,7 @@ declare @retorno      varchar(150)
 
         select @id_pais_nasc = b.ID_Pais
           from inserted     a with(nolock)
-          join tb_cidades   b with(nolock)on b.ID_Cidade = a.ID_Cidade_Nascimento
+ 	  join tb_cidades   b with(nolock)on b.ID_Cidade = a.ID_Cidade_Nascimento
          where a.ID_Cidade_Nascimento = @id_cid_nasc
 
     if @id_pais_nasc = @id_dupla_cid
@@ -28,22 +28,67 @@ declare @retorno      varchar(150)
        raiserror ('País de nascimento e de dupla cidadania informados são iguais.', 11, 127)
        rollback transaction
     end
-	
-    if len(@dt_nasc) > 10	
-    or try_convert(date, @dt_nasc) is null
+
+	/* validação do formato da data de nascimento */
+    if len(@dt_nasc) <> 10 and len(@dt_nasc) <> 4 and len(@dt_nasc) <> 7 
     begin
        raiserror ('Formato da data de nascimento é inválido.', 11, 127)
        rollback transaction
     end
 
-    if len(@dt_obt) > 10
-    or try_convert(date, @dt_obt) is null
+    if len(@dt_nasc) = 4
+    and try_convert(int, @dt_nasc) is null	
+    begin
+       raiserror ('Formato da data de nascimento é inválido.', 11, 127)
+       rollback transaction      
+    end
+
+    if len(@dt_nasc) = 7
+    and substring(@dt_nasc,3,1) <> '/'	
+    begin
+       raiserror ('Formato da data de nascimento é inválido.', 11, 127)
+       rollback transaction      
+    end
+
+    if len(@dt_nasc) = 10
+    and try_convert(date, @dt_nasc) is null	
+    begin
+       raiserror ('Formato da data de nascimento é inválido.', 11, 127)
+       rollback transaction      
+    end
+
+	/* validação do formato da data de óbito */
+    if len(@dt_obt) <> 10 and len(@dt_obt) <> 4 and len(@dt_obt) <> 7 
     begin
        raiserror ('Formato da data de óbito é inválido.', 11, 127)
        rollback transaction
     end
 
-    if @dt_nasc <> '01/01/1000' and @dt_obt <> '01/01/1000'
+    if len(@dt_obt) = 4
+    and try_convert(int, @dt_obt) is null	
+    begin
+       raiserror ('Formato da data de nascimento é inválido.', 11, 127)
+       rollback transaction      
+    end
+
+    if len(@dt_obt) = 7
+    and substring(@dt_obt,3,1) <> '/'	
+    begin
+       raiserror ('Formato da data de nascimento é inválido.', 11, 127)
+       rollback transaction      
+    end
+
+    if len(@dt_obt) = 10
+    and try_convert(date, @dt_obt) is null	
+    begin
+       raiserror ('Formato da data de nascimento é inválido.', 11, 127)
+       rollback transaction      
+    end
+
+	/* validando ordem cronológica */
+	/* a data 01/01/1000 é utilizada quando a data de nascimento ou óbito é desconhecida */
+    if (len(@dt_nasc) = 10 and @dt_nasc <> '01/01/1000') 
+    and (len(@dt_obt) = 10 and @dt_obt <> '01/01/1000')  
     begin
       if convert(date, @dt_nasc) > convert(date, @dt_obt)
       or convert(date, @dt_nasc) = convert(date, @dt_obt)
