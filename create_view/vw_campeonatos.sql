5b781ed65240a95ca5697643d4b7a2841554d31d
Vinicius Molon <molon.vinicius@gmail.com>
2024-09-13 15:21:26 -0300

    Create vw_campeonatos.sql

---
+++ create_view/vw_campeonatos.sql
@@ -0,0 +1,12 @@
+create view vw_campeonatos 
+
+as
+
+     select b.ID_Campeonato_Edicao  as ID_Campeonato_Edicao
+          , concat(b.Ano, ' ', a.Descricao, ' - ', c.Nome_Pais) as Nome
+          , b.Ano                   as Ano
+          , a.Descricao             as Campeonato
+          , c.Nome_Pais             as Pais_Sede     
+       from tb_campeonatos           a with(nolock)
+      join tb_campeonatos_edicoes   b with(nolock)on b.ID_Campeonato = a.ID_Campeonato
+      join tb_paises                c with(nolock)on c.ID_Pais = b.Pais_Sede
