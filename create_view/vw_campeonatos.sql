810f9dca3b4332645ab10042bd8c9509e2c53abc
Vinicius Molon <molon.vinicius@gmail.com>
2024-09-15 15:18:11 -0300

    Update vw_campeonatos.sql

--- create_view/vw_campeonatos.sql
+++ create_view/vw_campeonatos.sql
@@ -1,11 +1,11 @@
-create view vw_campeonatos 
+alter view vw_campeonatos 
 
 as
 
      select b.ID_Campeonato_Edicao  as ID_Campeonato_Edicao
           , concat(b.Ano, ' ', a.Descricao, ' - ', c.Nome_Pais) as Nome
           , b.Ano                   as Ano
-          , a.Descricao             as Campeonato
+          , a.Descricao             as Desc_Campeonato_Edicao
           , c.Nome_Pais             as Pais_Sede     
           , a.ID_Campeonato         as ID_Campeonato
           , a.Descricao             as Campeonato
