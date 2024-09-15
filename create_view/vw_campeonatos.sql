b641f59ee2ba82cf38525337541eec423e01e49e
Vinicius Molon <molon.vinicius@gmail.com>
2024-09-15 15:17:25 -0300

    Update vw_campeonatos.sql

--- create_view/vw_campeonatos.sql
+++ create_view/vw_campeonatos.sql
@@ -8,7 +8,7 @@ as
           , a.Descricao             as Campeonato
           , c.Nome_Pais             as Pais_Sede     
           , a.ID_Campeonato         as ID_Campeonato
-          , a.Descricao             as Desc_Tipo_Campeonato
+          , a.Descricao             as Campeonato
        from tb_campeonatos           a with(nolock)
        join tb_campeonatos_edicoes   b with(nolock)on b.ID_Campeonato = a.ID_Campeonato
        join tb_paises                c with(nolock)on c.ID_Pais = b.Pais_Sede
