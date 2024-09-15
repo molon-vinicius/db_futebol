250478bd26bd88de9f50df59ed1c733ddaf072c0
Vinicius Molon <molon.vinicius@gmail.com>
2024-09-15 15:11:52 -0300

    Update vw_campeonatos.sql

--- create_view/vw_campeonatos.sql
+++ create_view/vw_campeonatos.sql
@@ -5,8 +5,10 @@ as
      select b.ID_Campeonato_Edicao  as ID_Campeonato_Edicao
           , concat(b.Ano, ' ', a.Descricao, ' - ', c.Nome_Pais) as Nome
           , b.Ano                   as Ano
-          , a.Descricao             as Campeonato
-          , c.Nome_Pais             as Pais_Sede     
+		, a.Descricao             as Campeonato
+	     , c.Nome_Pais             as Pais_Sede     
+          , a.ID_Tipo_Campeonato    as ID_Tipo_Campeonato
+          , a.Descricao             as Desc_Tipo_Campeonato
        from tb_campeonatos           a with(nolock)
-      join tb_campeonatos_edicoes   b with(nolock)on b.ID_Campeonato = a.ID_Campeonato
-      join tb_paises                c with(nolock)on c.ID_Pais = b.Pais_Sede
+       join tb_campeonatos_edicoes   b with(nolock)on b.ID_Campeonato = a.ID_Campeonato
+	  join tb_paises                c with(nolock)on c.ID_Pais = b.Pais_Sede
