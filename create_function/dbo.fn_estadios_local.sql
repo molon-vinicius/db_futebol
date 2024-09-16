897ad9e24413b248652a984a2be3126ab9f049cd
Vinicius Molon <molon.vinicius@gmail.com>
2024-09-16 17:09:33 -0300

    Update dbo.fn_estadios_local.sql

--- create_function/dbo.fn_estadios_local.sql
+++ create_function/dbo.fn_estadios_local.sql
@@ -8,10 +8,10 @@ declare @cidade_estadio varchar(128)
 
      select @cidade_estadio = concat(b.Nome_Cidade, ' - ', c.Nome_Pais)
        from tb_estadios    a with(nolock)
-  	   join tb_cidades     b with(nolock)on b.ID_Cidade = a.ID_Cidade
-	     join tb_paises      c with(nolock)on c.ID_Pais = b.ID_Pais
+       join tb_cidades     b with(nolock)on b.ID_Cidade = a.ID_Cidade
+       join tb_paises      c with(nolock)on c.ID_Pais = b.ID_Pais
       where a.Nome_Reduzido = @nome_estadio
-	       or a.Nome_Estadio like '%'+@nome_estadio+'%' 
+         or a.Nome_Estadio like '%'+@nome_estadio+'%' 
 		
      return @cidade_estadio
        
