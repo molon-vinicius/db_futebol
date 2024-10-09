a197bdf101ff39513b32f1be8ce4ba235cc1a933
Vinicius Molon <molon.vinicius@gmail.com>
2024-10-09 13:16:42 -0300

    Update tb_selecoes_elencos_capitaes.sql

--- scripts_create_table/tb_selecoes_elencos_capitaes.sql
+++ scripts_create_table/tb_selecoes_elencos_capitaes.sql
@@ -4,4 +4,4 @@ create table tb_selecoes_elencos_capitaes
             ,ID_Selecao numeric(15)
             ,ID_Jogador numeric(15))
   
-alter table tb_selecoes_elencos_capitaes add constraint uq_sel_elenco_cap unique (ID_Jogo_Selecao, ID_Selecao, ID_Jogador)
+alter table tb_selecoes_elencos_capitaes add constraint uq_sel_elenco_cap unique (ID_Jogo_Selecao, ID_Selecao)
