54eca03680aafc5d4d5d5d7c91e7899df923c8c9
Vinicius Molon <molon.vinicius@gmail.com>
2024-10-10 16:20:24 -0300

    Update insert_into_tb_tipos_campeonatos.sql

--- scripts_insert/insert_into_tb_tipos_campeonatos.sql
+++ scripts_insert/insert_into_tb_tipos_campeonatos.sql
@@ -1,4 +1,10 @@
+set identity_insert tb_tipos_campeonatos on
+           
 insert into tb_tipos_campeonatos 
-           (Descricao)
-     values('Clubes')
-          ,('Seleções')
+           ( ID_Tipo_Campeonato
+           , Descricao)
+     values(0, 'Campeonato Não Oficial')
+          ,(1, 'Clubes')
+          ,(2, 'Seleções')
+
+set identity_insert tb_tipos_campeonatos off
