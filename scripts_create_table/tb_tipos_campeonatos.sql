2cadfb5cdad3eaa9b960a0d0dbb1e1ffacef9262
Vinicius Molon <molon.vinicius@gmail.com>
2024-10-10 16:18:37 -0300

    Update tb_tipos_campeonatos.sql

--- scripts_create_table/tb_tipos_campeonatos.sql
+++ scripts_create_table/tb_tipos_campeonatos.sql
@@ -1,3 +1,5 @@
-CREATE TABLE tb_tipos_campeonatos
-(ID_Tipo_Campeonato INT IDENTITY(1,1) PRIMARY KEY NOT NULL
-,Descricao VARCHAR(20) NOT NULL) 
+create table tb_tipos_campeonatos
+(ID_Tipo_Campeonato int identity(1,1) primary key not null
+,Descricao varchar(25) not null) 
+
+alter table tb_tipos_campeonatos add constraint uq_tipo_camp unique (Descricao) 
