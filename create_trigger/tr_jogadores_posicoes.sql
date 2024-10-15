32a6a5f7e3cefd376743c041ffd2e69265f1c5cd
Vinicius Molon <molon.vinicius@gmail.com>
2024-10-15 17:15:06 -0300

    Update tr_jogadores_posicoes.sql

--- create_trigger/tr_jogadores_posicoes.sql
+++ create_trigger/tr_jogadores_posicoes.sql
@@ -18,72 +18,99 @@ declare @sigla_posicao varchar(2)
 declare @cont tinyint = 1
 declare @comando nvarchar(max) = ''
 
- select @id_jogador = ID_jogador
-   from inserted
+    select @id_jogador = ID_jogador
+      from inserted
 
-if object_id('tempdb..#contadores') is not null drop table #contadores
-create table #contadores 
-             (cont_S tinyint
-			       ,cont_P tinyint)
-insert into  #contadores (cont_S, cont_P) values (0,0)
+    if object_id('tempdb..#contadores') is not null drop table #contadores
+    create table #contadores 
+                (cont_S tinyint
+               , cont_P tinyint)
+    insert into #contadores (cont_S, cont_P) values (0,0)
 
-while @cont <= @aux
-begin
+    while @cont <= @aux
+    begin
    
     select @Sigla_Posicao = Sigla_Posicao 
-	    from tb_posicoes with(nolock)
+      from tb_posicoes with(nolock)
      where ID_Posicao = @cont
 
        set @comando =
-    '
-     if exists ( select ID_Jogador from tb_jogadores_posicoes where ID_Jogador = '+convert(varchar(6),@id_jogador)+' and '+@Sigla_Posicao+' = ''S'')
-	 begin
-        update #contadores 
-           set cont_S = cont_S + 1
-	 end
+      '
+       if exists ( select ID_Jogador from tb_jogadores_posicoes where ID_Jogador = '+convert(varchar(6),@id_jogador)+' and '+@Sigla_Posicao+' = ''S'')
+	   begin
+          update #contadores 
+          set cont_S = cont_S + 1
+	   end
 
-	      if exists ( select ID_Jogador from tb_jogadores_posicoes where ID_Jogador = '+convert(varchar(6),@id_jogador)+' and '+@Sigla_Posicao+' = ''P'')
-	 begin
-        update #contadores 
-           set cont_P = cont_P + 1
-	 end
-	'
-	execute sp_executesql @comando 
+	   if exists ( select ID_Jogador from tb_jogadores_posicoes where ID_Jogador = '+convert(varchar(6),@id_jogador)+' and '+@Sigla_Posicao+' = ''P'')
+	   begin
+          update #contadores 
+          set cont_P = cont_P + 1
+	   end
+	 '
+	    execute sp_executesql @comando 
 
-	set @cont = @cont + 1
-end
-      
-if  (select cont_S from #contadores) = 0
-and (select cont_P from #contadores) = 0
-  begin
-    raiserror ('É necessário pelo menos uma posição cadastrada para salvar o registro na tabela.', 11, 127)
-    rollback transaction
-  end
+	    set @cont = @cont + 1
+     end
 
-if  (select cont_S from #contadores) > 1 
-and (select cont_P from #contadores) = 0
-  begin
-    raiserror ('É necessário definir uma posição como ''P'' [Preferencial] quando há mais de uma posição cadastrada.', 11, 127)
-    rollback transaction
-  end
+   if (select ID_Jogador from deleted) is not null
+   begin
+     if  (select cont_S from #contadores) = 0
+     and (select cont_P from #contadores) = 0
+     begin
+       raiserror ('É necessário pelo menos uma posição cadastrada para salvar o registro na tabela.', 11, 127)
+       rollback transaction
+     end
 
-if  (select cont_S from #contadores) = 0 
-and (select cont_P from #contadores) = 1
-  begin
-    raiserror ('Caso haja só uma posição cadastrada para o jogador, salvar com a instrução ''S''.', 11, 127)
-    rollback transaction
-  end
+     if  (select cont_S from #contadores) > 1 
+     and (select cont_P from #contadores) = 0
+     begin
+       raiserror ('É necessário definir uma posição como ''P'' [Preferencial] quando há mais de uma posição cadastrada.', 11, 127)
+       rollback transaction
+     end
 
-if  (select cont_S from #contadores) = 0
-and (select cont_P from #contadores) > 1
-  begin
-    raiserror ('Não é possível salvar múltiplas posições como ''P'' [Preferencial].', 11, 127)
-    rollback transaction
-  end
+     if  (select cont_S from #contadores) = 0 
+     and (select cont_P from #contadores) = 1
+     begin
+       raiserror ('Caso haja só uma posição cadastrada para o jogador, salvar com a instrução ''S''.', 11, 127)
+       rollback transaction
+     end
 
-if  (select cont_S from #contadores) > 1
-and (select cont_P from #contadores) > 1
-  begin
-    raiserror ('Não é possível salvar múltiplas posições como ''P'' [Preferencial].', 11, 127)
-    rollback transaction
-  end
+     if (select cont_P from #contadores) > 1
+     begin
+       raiserror ('Não é possível salvar múltiplas posições como ''P'' [Preferencial].', 11, 127)
+       rollback transaction
+     end
+
+   end
+     
+   if (select ID_Jogador from deleted) is null
+   begin
+     if  (select cont_S from #contadores) = 0
+     and (select cont_P from #contadores) = 0
+     begin
+       raiserror ('É necessário pelo menos uma posição cadastrada para salvar o registro na tabela.', 11, 127)
+       rollback transaction
+     end
+
+     if  (select cont_S from #contadores) > 1 
+     and (select cont_P from #contadores) = 0
+     begin
+       raiserror ('É necessário definir uma posição como ''P'' [Preferencial] quando há mais de uma posição cadastrada.', 11, 127)
+       rollback transaction
+     end
+
+     if  (select cont_S from #contadores) = 0 
+     and (select cont_P from #contadores) = 1
+     begin
+       raiserror ('Caso haja só uma posição cadastrada para o jogador, salvar com a instrução ''S''.', 11, 127)
+       rollback transaction
+     end
+
+     if (select cont_P from #contadores) > 1
+     begin
+       raiserror ('Não é possível salvar múltiplas posições como ''P'' [Preferencial].', 11, 127)
+       rollback transaction
+     end
+
+   end
