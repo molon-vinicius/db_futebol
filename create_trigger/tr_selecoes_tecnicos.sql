93d372e7e27be7fbf027a04f6beee0b577f3e64f
Vinicius Molon <molon.vinicius@gmail.com>
2024-10-15 14:09:00 -0300

    Update tr_selecoes_tecnicos.sql

--- create_trigger/tr_selecoes_tecnicos.sql
+++ create_trigger/tr_selecoes_tecnicos.sql
@@ -13,7 +13,9 @@ declare @id_tec     int
              , @id_sel      = ID_Selecao
              , @id_tec      = ID_Tecnico
           from inserted
-
+  
+  if (select ID_Selecao_Tecnico from deleted) is not null
+  begin
     if not exists (
            select ID_Selecao 
              from tb_campeonatos_edicoes_selecoes_part with(nolock)
@@ -33,6 +35,31 @@ declare @id_tec     int
        raiserror ('Técnico já cadastrado em outra seleção.', 11, 127)
        rollback transaction
     end
-	
+  end
+  
+  if (select ID_Selecao_Tecnico from deleted) is null
+  begin
+    if not exists (
+           select ID_Selecao 
+             from tb_campeonatos_edicoes_selecoes_part with(nolock)
+            where ID_Campeonato_Edicao = @id_camp_ed
+              and ID_Selecao = @id_sel
+    )              
+    begin
+       raiserror ('Seleção não participou desta edição da competição.', 11, 127)
+       rollback transaction
+    end
+
+    if (select count(ID_Selecao_Tecnico) as qtd 
+          from tb_selecoes_tecnicos with(nolock)
+         where ID_Campeonato_Edicao = @id_camp_ed
+           and ID_Tecnico = @id_tec) > 1
+    begin
+       raiserror ('Técnico já cadastrado em outra seleção.', 11, 127)
+       rollback transaction
+    end
+
+  end
+  
 end
 
