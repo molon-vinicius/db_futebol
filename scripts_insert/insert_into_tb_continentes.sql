307fa4fe97b5d3671fe867edc11130ada36337cd
Vinicius Molon <molon.vinicius@gmail.com>
2024-10-16 00:56:15 -0300

    Update insert_into_tb_continentes.sql

--- scripts_insert/insert_into_tb_continentes.sql
+++ scripts_insert/insert_into_tb_continentes.sql
@@ -4,10 +4,10 @@ set identity_insert tb_continentes on
 insert into tb_continentes (ID_Continente, Nome_Continente)
      values (0,'Todos')
           , (1,'África')
-	    	  , (2,'Ásia')
-	    	  , (3,'América Central e do Norte')
-	    	  , (4,'América do Sul')
-    		  , (5,'Europa')
-		      , (6,'Oceania')
+          , (2,'Ásia')
+          , (3,'América Central e do Norte')
+          , (4,'América do Sul')
+          , (5,'Europa')
+          , (6,'Oceania')
 
 set identity_insert tb_continentes off
