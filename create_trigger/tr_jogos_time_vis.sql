52f6afbbb9f6b0c16bd55c593d82d0b0aa52c4c2
Vinicius Molon <molon.vinicius@gmail.com>
2024-10-16 17:17:38 -0300

    Update tr_jogos_time_vis.sql

--- create_trigger/tr_jogos_time_vis.sql
+++ create_trigger/tr_jogos_time_vis.sql
@@ -11,12 +11,24 @@ declare @qtd_jogadores int
 
 	   select @qtd_jogadores = count(a.ID_Jogador)
 	     from tb_jogos_times_visitantes a with(nolock)
-	  	where a.ID_Jogo_Time = @id
-  
+            where a.ID_Jogo_Time = @id
+
+if (select ID_Jogo_Time from deleted) is not null
+begin 
   if @qtd_jogadores > 11
   begin
      raiserror ('Quantidade de jogadores titulares já atingida.', 11, 127)
-	   rollback transaction
+     rollback transaction
   end
+end
+
+if (select ID_Jogo_Time from deleted) is null
+begin
+  if @qtd_jogadores > 11
+  begin
+     raiserror ('Quantidade de jogadores titulares já atingida.', 11, 127)
+     rollback transaction
+  end
+end
 
 end
