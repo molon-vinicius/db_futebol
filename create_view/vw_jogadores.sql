2c53641caf512e67049050276912fc1a5e262948
Vinicius Molon <molon.vinicius@gmail.com>
2024-11-07 15:41:05 -0300

    Update vw_jogadores.sql
    Correção para jogadores que não tem a cidade nascimento, trazendo o país preferencial.

--- create_view/vw_jogadores.sql
+++ create_view/vw_jogadores.sql
@@ -1,20 +1,27 @@
-create view vw_jogadores 
+alter view vw_jogadores 
   
 as  
-  select a.ID_Jogador
-       , b.Nome_Reduzido  
-       , dbo.fn_pessoas_nacionalidade(b.Nome_Completo) as Nacionalidade
-       , case when Pe_Preferencial = 'R'
-              then 'Destro'
-              when Pe_Preferencial = 'L'
-              then 'Canhoto'
-              when Ambidestro = 'S'
-              then 'Ambidestro'
-              else 'Sem informação'
-         end                                           as Pe_Preferencial       
-       , a.Dois_Lados
-       , a.Lado_Oposto       	   
-    from tb_jogadores    a with(nolock)  
-    join tb_pessoas      b with(nolock)on b.ID_Pessoa = a.ID_Pessoa  
+   select a.ID_Jogador
+        , b.Nome_Reduzido  
+        , dbo.fn_pessoas_nacionalidade(b.Nome_Completo) as Nacionalidade
+        , isnull(c.Nome_Pais,d.Nome_Pais)               as Pais_Nascimento
+        , dbo.fn_jogadores_posicoes(a.ID_Jogador)       as Posicoes
+        , case when Ambidestro = 'S'
+               then 'Ambidestro'
+               when Pe_Preferencial = 'R'
+               then 'Destro'
+               when Pe_Preferencial = 'L'
+               then 'Canhoto'
+               when Ambidestro = 'S'
+               then 'Ambidestro'
+               else ' - '
+          end                                           as Pe_Preferencial       
+        , a.Dois_Lados
+        , a.Lado_Oposto       	   
+     from tb_jogadores    a with(nolock)  
+     join tb_pessoas      b with(nolock)on b.ID_Pessoa = a.ID_Pessoa 
+left join vw_cidades      c with(nolock)on c.ID_Cidade = b.ID_Cidade_Nascimento
+     join tb_paises       d with(nolock)on d.ID_Pais   = b.Pais_Preferencial
+
 
 
