7a4778afffb5cc706ea3642ab939ffcb852b58e0
Vinicius Molon <molon.vinicius@gmail.com>
2024-12-03 19:32:48 -0300

    Update vw_jogos_selecoes_eventos.sql

--- create_view/vw_jogos_selecoes_eventos.sql
+++ create_view/vw_jogos_selecoes_eventos.sql
@@ -1,14 +1,19 @@
-create view vw_jogos_selecoes_eventos
-
-as
-
-     select a.ID_jogo_selecao 
-          , a.minuto
-          , b.Descricao       as evento
-          , c.Nome_Selecao
-          , d.Nome_Reduzido   as jogador
-          , a.observacao
-       from tb_jogos_selecoes_eventos a with(nolock)
-       join tb_tipos_eventos          b with(nolock)on b.ID_tipo_evento = a.ID_tipo_evento
-       join tb_selecoes               c with(nolock)on c.ID_Selecao = a.ID_selecao
-       join vw_jogadores              d with(nolock)on d.ID_jogador = a.ID_jogador
+alter view vw_jogos_selecoes_eventos  
+  
+as  
+  
+     select a.ID_jogo_selecao
+          , x.Data_Jogo
+          , x.Descricao_Completa  as campeonato
+          , x.Fase
+          , iif(c.ID_Selecao = x.ID_Anfitriao, Visitante, Anfitriao) as adversario
+          , a.minuto  
+          , b.Descricao           as evento  
+          , c.Nome_Selecao  
+          , d.Nome_Reduzido       as jogador  
+          , a.observacao  
+       from vw_jogos_selecoes         x with(nolock)
+       join tb_jogos_selecoes_eventos a with(nolock)on a.ID_Jogo_Selecao = x.ID_Jogo_Selecao  
+       join tb_tipos_eventos          b with(nolock)on b.ID_tipo_evento = a.ID_tipo_evento  
+       join tb_selecoes               c with(nolock)on c.ID_Selecao = a.ID_selecao  
+       join vw_jogadores              d with(nolock)on d.ID_jogador = a.ID_jogador  
