92130a627d2c217250d303b9779c19a508b8b9f9
Vinicius Molon <molon.vinicius@gmail.com>
2024-12-04 14:12:29 -0300

    Update vw_campeonatos.sql

--- create_view/vw_campeonatos.sql
+++ create_view/vw_campeonatos.sql
@@ -1,14 +1,22 @@
-alter view vw_campeonatos 
+create view vw_campeonatos
 
-as
+as 
 
-     select b.ID_Campeonato_Edicao  as ID_Campeonato_Edicao
-          , concat(b.Ano, ' ', a.Descricao, ' - ', c.Nome_Pais) as Nome
-          , b.Ano                   as Ano
-          , a.Descricao             as Desc_Campeonato_Edicao
-          , c.Nome_Pais             as Pais_Sede     
-          , a.ID_Campeonato         as ID_Campeonato
-          , a.Descricao             as Campeonato
-       from tb_campeonatos           a with(nolock)
-       join tb_campeonatos_edicoes   b with(nolock)on b.ID_Campeonato = a.ID_Campeonato
-       join tb_paises                c with(nolock)on c.ID_Pais = b.Pais_Sede
+    select a.ID_Campeonato         
+         , b.ID_Campeonato_Edicao
+         , a.Descricao        as Campeonato
+         , b.Ano
+         , c.Nome_Pais        as Pais_Sede
+         , case when a.Nacional = 'S'
+                then 'Nacional'
+                when a.ID_Confederacao = 0 and a.ID_Tipo_Campeonato is not null
+                then 'Mundial'
+                when a.Nacional = 'N' and a.ID_Confederacao > 0
+                then 'Continental'
+                when a.ID_Tipo_Campeonato is null
+                then 'Amistoso'
+           end                as Nivel 
+      from tb_campeonatos           a with(nolock)
+      join tb_campeonatos_edicoes   b with(nolock)on b.ID_Campeonato = a.ID_Campeonato
+ left join tb_paises                c with(nolock)on c.ID_Pais = b.Pais_Sede
+ 
