e67cdfd45e8baa6c41279115cbf87c83d9f3ee00
Vinicius Molon <molon.vinicius@gmail.com>
2025-01-20 09:22:23 -0300

    Create vw_campeonatos_sel_part.sql

---
+++ create_view/vw_campeonatos_sel_part.sql
@@ -0,0 +1,12 @@
+create view vw_campeonatos_sel_part
+
+as
+
+     select a.ID_Campeonato
+          , a.ID_Campeonato_Edicao
+          , concat(a.Ano, ' ', a.Campeonato, ' - ', a.Pais_Sede) as Campeonato
+          , b.ID_selecao
+          , c.Nome_Selecao
+       from vw_campeonatos                       a with(nolock)
+       join tb_campeonatos_edicoes_selecoes_part b with(nolock)on b.ID_campeonato_edicao = a.ID_Campeonato_Edicao
+       join tb_selecoes                          c with(nolock)on c.ID_Selecao = b.ID_selecao     
