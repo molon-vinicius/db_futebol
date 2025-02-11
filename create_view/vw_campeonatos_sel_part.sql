b5f1d712439031dd6e61e76f401bd9f699ada038
Vinicius Molon <molon.vinicius@gmail.com>
2025-02-11 10:34:23 -0300

    Update vw_campeonatos_sel_part.sql
    adicionada a coluna 'Grupos' para facilitar na ordenação

--- create_view/vw_campeonatos_sel_part.sql
+++ create_view/vw_campeonatos_sel_part.sql
@@ -7,6 +7,7 @@ as
           , concat(a.Ano, ' ', a.Campeonato, ' - ', a.Pais_Sede) as Campeonato
           , b.ID_selecao
           , c.Nome_Selecao
+          , b.Grupos     
        from vw_campeonatos                       a with(nolock)
        join tb_campeonatos_edicoes_selecoes_part b with(nolock)on b.ID_campeonato_edicao = a.ID_Campeonato_Edicao
        join tb_selecoes                          c with(nolock)on c.ID_Selecao = b.ID_selecao     
