3ea3089f20ba17f8b2568c2bf1273732b8920df8
Vinicius Molon <molon.vinicius@gmail.com>
2025-03-09 17:34:00 -0300

    Update tb_times_historicos.sql

--- scripts_create_table/tb_times_historicos.sql
+++ scripts_create_table/tb_times_historicos.sql
@@ -1,11 +1,11 @@
  create table tb_times_historicos
              (ID_Time_Historico numeric(15) identity primary key
-      			 ,ID_Time numeric(15)
-			       ,Nome_Time varchar(128)
-			       ,Nome_Reduzido varchar(60)
-			       ,ID_Cidade int
-			       ,Data_Inicio varchar(10)
-			       ,Data_Fim varchar(10))
+             ,ID_Time numeric(15)
+             ,Nome_Time varchar(128)
+             ,Nome_Reduzido varchar(60)
+             ,ID_Cidade int
+             ,Data_Inicio varchar(10)
+             ,Data_Fim varchar(10))
 
 alter table tb_times_historicos 
 add constraint fk_times_hist_id_time
@@ -14,3 +14,11 @@ foreign key (ID_Time) references tb_times(ID_Time)
 alter table tb_times_historicos 
 add constraint fk_times_hist_id_cidade
 foreign key (ID_Cidade) references tb_cidades(ID_Cidade)
+
+alter table tb_times_historicos
+add ID_Status_Historico int
+
+alter table tb_times_historicos 
+add constraint fk_times_hist_id_status
+foreign key (ID_Status_Historico) references tb_status_historico(ID_Status_Historico)
+
