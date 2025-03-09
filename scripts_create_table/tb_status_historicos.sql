4a0ef1c09938b6a9644b5fc93200dbf1224e8eb6
Vinicius Molon <molon.vinicius@gmail.com>
2025-03-09 17:52:40 -0300

    Update tb_status_historicos.sql

--- scripts_create_table/tb_status_historicos.sql
+++ scripts_create_table/tb_status_historicos.sql
@@ -1,3 +1,4 @@
 create table tb_status_historicos
             (ID_Status_Historico int identity primary key
-            ,Descricao nvarchar(60))
+            ,Descricao nvarchar(60)
+            ,Observacao nvarchar(500))
