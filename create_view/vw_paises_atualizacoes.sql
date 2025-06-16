02a49f66c3a429d162e911386f776c0bcdc89099
Vinicius Molon <molon.vinicius@gmail.com>
2025-06-16 16:10:05 -0300

    Create vw_paises_atualizacoes.sql

---
+++ create_view/vw_paises_atualizacoes.sql
@@ -0,0 +1,13 @@
+create or alter view vw_paises_atualizacoes
+
+as
+
+    select a.ID_Pais_Antigo       as ID_Pais_Antigo
+         , b.Nome_Pais            as Nome_Pais_Antigo
+         , a.Inicio_Vigencia      as Inicio_Vigencia
+         , a.Fim_Vigencia         as Fim_Vigencia
+         , a.ID_Pais_Atual        as ID_Pais_Atual
+         , c.Nome_Pais            as Nome_Pais_Atual
+      from tb_paises_atualizacoes   a with(nolock)
+      join tb_paises                b with(nolock)on b.ID_Pais = a.ID_Pais_Antigo
+      join tb_paises                c with(nolock)on c.ID_Pais = a.ID_Pais_Atual
