ad2f84ef8eab841768589a0282ca4eb6f4952b82
Vinicius Molon <molon.vinicius@gmail.com>
2025-08-01 13:05:20 -0300

    Update vw_arbitros.sql

--- create_view/vw_arbitros.sql
+++ create_view/vw_arbitros.sql
@@ -3,6 +3,6 @@ create view vw_arbitros
 as  
      select a.ID_Arbitro  
           , b.Nome_Reduzido  
-          , dbo.fn_pessoas_nacionalidade(b.Nome_Reduzido) as Nacionalidade  
+          , dbo.fn_pessoas_nacionalidade(b.ID_Pessoa) as Nacionalidade  
        from tb_arbitros     a with(nolock)  
        join tb_pessoas      b with(nolock)on b.ID_Pessoa = a.ID_Pessoa  
