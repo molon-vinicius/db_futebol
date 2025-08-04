05b5908a878f0afc8bd974279c20ad0278405298
Vinicius Molon <molon.vinicius@gmail.com>
2025-08-04 12:29:38 -0300

    Update tb_tipos_eventos.sql

--- scripts_create_table/tb_tipos_eventos.sql
+++ scripts_create_table/tb_tipos_eventos.sql
@@ -3,3 +3,16 @@ CREATE TABLE tb_tipos_eventos
 ,Descricao VARCHAR(30) NOT NULL)
 
 GO
+
+INSERT INTO tb_tipos_eventos
+           (Descricao)
+    VALUES (Gol)
+          ,(Cartão Amarelo)
+          ,(Cartão Vermelho)
+          ,(Gol Olímpico)
+          ,(Gol (P))
+          ,(Pênalti (X))
+          ,(Gol Anulado)
+          ,(Gol Contra)
+          ,(Gol de Falta)
+
