c6d33ab79f57277846efb335b0d09e97f491b890
Vinicius Molon <molon.vinicius@gmail.com>
2025-09-17 16:25:58 -0300

    Update tr_camp_edicoes.sql

--- create_trigger/tr_camp_edicoes.sql
+++ create_trigger/tr_camp_edicoes.sql
@@ -1,78 +1,31 @@
-create trigger tr_camp_edicoes
-            on tb_campeonatos_edicoes
+alter trigger tr_camp_edicoes
+on tb_campeonatos_edicoes
 for insert, update
-
 as
-
 begin
+    set nocount on
 
-declare @temporada varchar(9)
-declare @retorno varchar(150)
-declare @temporada_aux int
+    if exists (select 1 from deleted)
+    begin
+        raiserror('Não é possível alterar os dados nessa tabela. Necessário excluir e inserir novamente as informações.', 11, 27);
+        rollback transaction
+        return
+    end
 
-    select @temporada = Ano 
-      from inserted 
-
-  if (select ID_Campeonato_Edicao from deleted) is not null
-  begin
-    raiserror('Não é possível alterar os dados nessa tabela. Necessário excluir e inserir novamente as informações', 11, 27)
-    rollback tran
-  end
-
-  if (select ID_Campeonato_Edicao from deleted) is null
-  begin
-     if len(@temporada) = 4
-     begin
-	   set @temporada_aux = convert(int, @temporada)
-     end
-
-     if len(@temporada) = 9
-     begin
-	   set @temporada_aux = convert(int, substring(@temporada,6,4))
-     end
-
-     if (len(@temporada) <> 4 and len(@temporada) <> 9)
-     begin
-       raiserror ('A coluna ''Temporada'' deve estar no formato ''XXXX/XXXX'' ou ''XXXX''', 11, 127)
-       rollback transaction
-     end
-
-     if (len(@temporada) = 4 or len(@temporada) = 9)
-     begin
-        if len(@temporada) = 4
-        and (select dbo.fn_valida_numericos(@temporada)) is not null
-        begin
-              set @retorno = (select dbo.fn_valida_numericos(@temporada))
-              raiserror (@retorno, 11, 127)
-	      rollback transaction       
-        end
-        if len(@temporada) = 9
-	begin
-          if isnumeric(substring(@temporada,0,5)) = 0
-	  or isnumeric(substring(@temporada,6,4)) = 0	 
-	  begin
-               raiserror ('A coluna ''Temporada'' deve estar no formato ''XXXX/XXXX''', 11, 127)
-	       rollback transaction
-	  end
-          if isnumeric(substring(@temporada,0,5)) = 1
-	  and isnumeric(substring(@temporada,6,4)) = 1
-	  begin
-	        if substring(@temporada,5,1) <> '/'
-                begin
-	          raiserror ('A coluna ''Temporada'' deve estar no formato ''XXXX/XXXX''', 11, 127)
-	          rollback transaction
-                end
-	        if substring(@temporada,6,4) <> substring(@temporada,0,5)+1
-		begin
-                  set @retorno = concat('A temporada ',substring(@temporada,0,5),'/',substring(@temporada,6,4),' é inválida pois não é sequencial.')
-	          raiserror (@retorno, 11, 127)
-	          rollback transaction
-		end
-          end
-        end 
-     end
-
-  end
-
+    if exists (
+        select 1
+        from inserted i
+        where not (
+              (len(i.Ano) = 4  and i.Ano not like '%[^0-9]%') 
+           or (len(i.Ano) = 9 
+               and i.Ano like '[0-9][0-9][0-9][0-9]/[0-9][0-9][0-9][0-9]'
+               and cast(left(i.Ano,4) as int) + 1 = cast(right(i.Ano,4) as int))
+        )
+    )
+    begin
+        raiserror('A coluna "Temporada" deve estar no formato "XXXX" ou "XXXX/XXXX" (anos numéricos e consecutivos).', 11, 127)
+        rollback transaction
+        return
+    end
 end
 
