df9ab34ad66624f91649c18854cad7cecf8f3a66
Vinicius Molon <molon.vinicius@gmail.com>
2025-09-17 16:43:32 -0300

    Update tr_times_hist.sql

--- create_trigger/tr_times_hist.sql
+++ create_trigger/tr_times_hist.sql
@@ -1,142 +1,88 @@
-create trigger tr_times_hist
-            on tb_times_historicos
-for insert, update 
-
+alter trigger tr_times_hist
+           on tb_times_historicos
+for insert, update
 as
-
 begin
-declare @id_time        int
-declare @id_cidade      int
-declare @id_status_hist int
-declare @sit_atual      char
-declare @dt_ini         date
-declare @dt_fim         date
+    set nocount on
 
-        select @id_time        = ID_Time
-             , @id_cidade      = ID_Cidade
-             , @id_status_hist = ID_Status_Historico
-             , @sit_atual      = Situacao_Atual
-             , @dt_ini         = Data_Inicio
-             , @dt_fim         = Data_Fim
-          from inserted
-
-  if (select ID_Time_Historico from deleted) is not null
-  begin
-    if @sit_atual = 'S'
-    and @dt_fim is not null
+    if exists (
+       select 1
+         from inserted i
+        where i.Situacao_Atual = 'S'
+          and i.Data_Fim is not null
+    )
     begin
-       raiserror ('Se a coluna "Situação Atual" for igual a ''S'' a coluna "Data Fim" não pode estar preenchida.', 11, 127)
-       rollback transaction
+        raiserror('Se a coluna "Situação Atual" for igual a ''S'' a coluna "Data Fim" não pode estar preenchida.', 11, 127)
+        rollback transaction
+        return
     end
 
-    if @sit_atual = 'N'
-    and @dt_fim is null
+    if exists (
+      select 1
+        from inserted i
+       where i.Situacao_Atual = 'N'
+         and i.Data_Fim is null
+    )
     begin
-       raiserror ('Se a coluna "Situação Atual" for igual a ''N'' a coluna "Data Fim" não pode ser nula.', 11, 127)
-       rollback transaction
+        raiserror('Se a coluna "Situação Atual" for igual a ''N'' a coluna "Data Fim" não pode ser nula.', 11, 127)
+        rollback transaction
+        return
     end
 
-    if @sit_atual <> 'S'
-    and @dt_ini > @dt_fim
+    if exists (
+       select 1
+         from inserted i
+        where i.Situacao_Atual <> 'S'
+          and i.Data_Inicio > i.Data_Fim
+    )
     begin
-       raiserror ('Data Inicial não pode ser posterior a data final.', 11, 127)
-       rollback transaction
+        raiserror('Data Inicial não pode ser posterior a data final.', 11, 127)
+        rollback transaction
+        return
     end
 
-    if (select count(ID_Time_Historico) as qtd 
-          from tb_times_historicos with(nolock)
-         where ID_Time = @id_time
-           and ID_Status_Historico = 0) > 1
+    if exists (
+       select i.ID_Time
+         from inserted i
+         join tb_times_historicos th on th.ID_Time = i.ID_Time
+        where th.ID_Status_Historico = 0
+        group by i.ID_Time
+       having count(*) > 1
+    )
     begin
-       raiserror ('Time não pode ter o "Status Histórico" = 0 [Fundação] em mais de um registro.', 11, 127)
-       rollback transaction
+        raiserror('Time não pode ter o "Status Histórico" = 0 [Fundação] em mais de um registro.', 11, 127)
+        rollback transaction
+        return
     end
 
-    if (select count(ID_Time_Historico) as qtd 
-          from tb_times_historicos with(nolock)
-         where ID_Time = @id_time
-           and Situacao_Atual = 'S') > 1
+    if exists (
+        select i.ID_Time
+        from inserted i
+        join tb_times_historicos th on th.ID_Time = i.ID_Time
+        where th.Situacao_Atual = 'S'
+        group by i.ID_Time
+        having count(*) > 1
+    )
     begin
-       raiserror ('Time não pode ter mais de um registro com a "Situação Atual" = ''S''.', 11, 127)
-       rollback transaction
+        raiserror('Time não pode ter mais de um registro com a "Situação Atual" = ''S''.', 11, 127)
+        rollback transaction
+        return
     end
 
-    if (select b.ID_Pais
-          from tb_cidades a with(nolock)
-          join tb_paises  b with(nolock)on b.ID_Pais = a.ID_Pais
-         where a.ID_Cidade = @id_cidade
-	  ) <>
-	     (select b.ID_Pais
-          from tb_times   a with(nolock)
-          join tb_cidades b with(nolock)on b.ID_Cidade = a.ID_Cidade
-          join tb_paises  c with(nolock)on c.ID_Pais = b.ID_Pais
-         where a.ID_Time = @id_time
-	  )  
+    if exists (
+      select 1
+        from inserted i
+        join tb_cidades   c on c.ID_Cidade = i.ID_Cidade
+        join tb_paises   p1 on p1.ID_Pais = c.ID_Pais
+        join tb_times     t on t.ID_Time = i.ID_Time
+        join tb_cidades  c2 on c2.ID_Cidade = t.ID_Cidade
+        join tb_paises   p2 on p2.ID_Pais = c2.ID_Pais
+       where p1.ID_Pais <> p2.ID_Pais
+    )
     begin
-       raiserror ('A cidade informada deve ser do mesmo país cadastrado na tabela referência "tb_times".', 11, 127)
-       rollback transaction
+        raiserror('A cidade informada deve ser do mesmo país cadastrado na tabela referência "tb_times".', 11, 127)
+        rollback transaction
+        return
     end
-
-  end
-  
-  if (select ID_Time_Historico from deleted) is null
-  begin
-    if @sit_atual = 'S'
-    and @dt_fim is not null
-    begin
-       raiserror ('Se a coluna "Situação Atual" for igual a ''S'' a coluna "Data Fim" não pode estar preenchida.', 11, 127)
-       rollback transaction
-    end
-
-    if @sit_atual = 'N'
-    and @dt_fim is null
-    begin
-       raiserror ('Se a coluna "Situação Atual" for igual a ''N'' a coluna "Data Fim" não pode ser nula.', 11, 127)
-       rollback transaction
-    end
-
-    if @sit_atual <> 'S'
-    and @dt_ini > @dt_fim
-    begin
-       raiserror ('Data Inicial não pode ser posterior a data final.', 11, 127)
-       rollback transaction
-    end
-
-    if (select count(ID_Time_Historico) as qtd 
-          from tb_times_historicos with(nolock)
-         where ID_Time = @id_time
-           and ID_Status_Historico = 0) > 1
-    begin
-       raiserror ('Time não pode ter o "Status Histórico" = 0 [Fundação] em mais de um registro.', 11, 127)
-       rollback transaction
-    end
-
-    if (select count(ID_Time_Historico) as qtd 
-          from tb_times_historicos with(nolock)
-         where ID_Time = @id_time
-           and Situacao_Atual = 'S') > 1
-    begin
-       raiserror ('Time não pode ter mais de um registro com a "Situação Atual" = ''S''.', 11, 127)
-       rollback transaction
-    end
-
-    if (select b.ID_Pais
-          from tb_cidades a with(nolock)
-          join tb_paises  b with(nolock)on b.ID_Pais = a.ID_Pais
-         where a.ID_Cidade = @id_cidade
-    ) <>
-       (select b.ID_Pais
-          from tb_times   a with(nolock)
-          join tb_cidades b with(nolock)on b.ID_Cidade = a.ID_Cidade
-          join tb_paises  c with(nolock)on c.ID_Pais = b.ID_Pais
-         where a.ID_Time = @id_time
-    )  
-    begin
-       raiserror ('A cidade informada deve ser do mesmo país cadastrado na tabela referência "tb_times".', 11, 127)
-       rollback transaction
-    end
-  end
-  
 end
-
 
