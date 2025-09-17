8a0f3f0b718f034fc2de4a323647cb9669de06e0
Vinicius Molon <molon.vinicius@gmail.com>
2025-09-17 16:32:09 -0300

    Update tr_cidades_estados.sql

--- create_trigger/tr_cidades_estados.sql
+++ create_trigger/tr_cidades_estados.sql
@@ -1,93 +1,40 @@
-/* A priori só coloquei a validação de obrigação de preenchimento da coluna 'ID_Estado' para cidades do Brasil, que são as únicas informações da tabela referência
-até então. Posteriormente, caso haja interesse de cadastramento de estados de outros países, a trigger abaixo irá validar se o estado informado corresponde ao país
-no momento do INSERT ou UPDATE. Para tornar obrigatório o cadastramento de estados, basta alterar o primeiro bloco 'IF', trocando o '=' por IN, adicionando o nome 
-dos países desejados */
-
-create trigger tr_cidades_estados on tb_cidades
-for insert, update 
- 
+alter trigger tr_cidades_estados
+on tb_cidades
+for insert, update
 as
-
 begin
+    set nocount on
 
-declare @ID_Pais     int
-declare @ID_Estado   int
-declare @Nome_Cidade varchar(120)
-declare @Nome_Pais   varchar(100)  
-declare @old_ID_Pais     int
-declare @old_ID_Estado   int
+    /*
+       1. Se país 'Brasil', então ID_Estado é obrigatório.
+       2. Se ID_Estado informado, ele deve pertencer ao país informado.
+    */
 
-    select @ID_Pais     = b.ID_Pais
-         , @ID_Estado   = a.ID_Estado
-	 , @Nome_Cidade = a.Nome_Cidade
-         , @Nome_Pais   = b.Nome_Pais
-      from inserted    a with(nolock)
-      join tb_paises   b with(nolock)on b.ID_Pais = a.ID_Pais
+    if exists (
+       select 1
+         from inserted     i
+         join tb_paises    p on p.ID_Pais = i.ID_Pais
+        where p.Nome_Pais = 'Brasil'
+          and i.ID_Estado IS NULL
+    )
+    begin
+        raiserror('Para cadastrar cidades do Brasil é necessário informar o Estado pertencente.', 11, 127)
+        rollback transaction
+        return
+    end
 
-  if (select ID_Estado from deleted) is not null
-  begin
-    select @old_ID_Pais = a.ID_Pais
-         , @old_ID_Estado = a.id_estado
-      from deleted     a with(nolock)
-      join tb_paises   b with(nolock)on b.ID_Pais = a.ID_Pais
-	  
-        if @old_ID_Pais <> @ID_Pais
-        begin   
-
-	   if @Nome_Pais = 'Brasil'
-           begin
-              if @ID_Estado is null
-              begin
-                raiserror ('Para cadastrar cidades do Brasil é necessário informar o Estado pertencente.', 11, 127)
-	        rollback transaction
-              end
-           end
-
-	end
-
-	if @old_ID_Estado <> @ID_Estado
-        begin
-           if @ID_Estado is not null
-           begin
-              if not exists 
-              (  select b.ID_Pais
-                   from tb_estados a with(nolock)
-                   join tb_paises  b with(nolock)on b.ID_Pais = a.ID_Pais
-                  where a.ID_Estado = @ID_Estado
-                    and b.ID_Pais = @ID_Pais  )
-              begin
-                raiserror ('Estado não pertencente ao país informado.', 11, 127)
-	        rollback transaction
-              end
-           end
-        end
-  end
-
-  if (select ID_Estado from deleted) is null
-  begin
-     if @Nome_Pais = 'Brasil'
-     begin
-        if @ID_Estado is null
-        begin
-           raiserror ('Para cadastrar cidades do Brasil é necessário informar o Estado pertencente.', 11, 127)
-	   rollback transaction
-        end
-     end
-
-     if @ID_Estado is not null
-     begin
-        if not exists 
-        (  select b.ID_Pais
-             from tb_estados a with(nolock)
-             join tb_paises  b with(nolock)on b.ID_Pais = a.ID_Pais
-            where a.ID_Estado = @ID_Estado
-              and b.ID_Pais = @ID_Pais  )
-        begin
-          raiserror ('Estado não pertencente ao país informado.', 11, 127)
-	  rollback transaction
-        end
-     end
-
-  end
-
+    if exists (
+        select 1
+        from inserted    i
+        join tb_paises   p on p.ID_Pais = i.ID_Pais
+   left join tb_estados  e on e.ID_Estado = i.ID_Estado
+       where i.ID_Estado is not null
+         and (e.ID_Estado is null or e.ID_Pais <> i.ID_Pais)
+    )
+    begin
+        raiserror('Estado não pertencente ao país informado.', 11, 127)
+        rollback transaction
+        return
+    end
 end
+
