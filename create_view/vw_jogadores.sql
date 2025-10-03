56492cb5efcad9f26f41ced71a8db2de24fb1163
Vinicius Molon <molon.vinicius@gmail.com>
2025-10-03 15:10:41 -0300

    Update vw_jogadores.sql

--- create_view/vw_jogadores.sql
+++ create_view/vw_jogadores.sql
@@ -1,31 +1,117 @@
-create or alter view vw_jogadores 
-  
-as  
-   select b.ID_Pessoa
-        , a.ID_Jogador
-        , b.Nome_Completo
-        , b.Nome_Reduzido
-        , b.Altura
-        , b.Data_Nascimento
-        , dbo.fn_pessoas_nacionalidade(b.ID_Pessoa)     as Nacionalidade
-        , isnull(c.Nome_Cidade,'Não Informado')         as Cidade_Nascimento
-        , isnull(c.Nome_Pais,d.Nome_Pais)               as Pais_Nascimento
-        , dbo.fn_jogadores_posicoes(a.ID_Jogador)       as Posicoes
-        , case when Ambidestro = 'S'
-               then 'Ambidestro'
-               when Pe_Preferencial = 'R'
-               then 'Destro'
-               when Pe_Preferencial = 'L'
-               then 'Canhoto'
-               when Ambidestro = 'S'
-               then 'Ambidestro'
-               else ' - '
-          end                                           as Pe_Preferencial       
-        , a.Dois_Lados
-        , a.Lado_Oposto       	   
-     from tb_jogadores    a with(nolock)  
-     join tb_pessoas      b with(nolock)on b.ID_Pessoa = a.ID_Pessoa 
-left join vw_cidades      c with(nolock)on c.ID_Cidade = b.ID_Cidade_Nascimento
-     join tb_paises       d with(nolock)on d.ID_Pais   = b.Pais_Preferencial
+create view vw_jogos_selecoes
 
-	    
+as
+
+       select distinct
+              src.ID_jogo_selecao                  as ID_jogo_selecao 
+            , format(src.Data_Jogo, 'dd/MM/yyyy')  as Data_Jogo
+            , concat(ce.Ano, ' ', c.Descricao, ' - ', p.Nome_Pais ) as Descricao_Completa
+			, est.Nome_Reduzido                    as Estadio
+            , dbo.fn_estadios_local(est.Nome_Reduzido) as Local_Estadio
+            , src.Espectadores                     as Espectadores
+			, src.ID_Campeonato_Fase               as ID_Fase
+			, dbo.fn_nome_fase(src.ID_Jogo_Selecao) as Fase
+			, anf.ID_Selecao                       as ID_Anfitriao
+			, anf.Nome_Selecao                     as Anfitriao
+            , convert(varchar(12), case when pen_anf.ID_jogo_selecao is not null
+                                        then '(' + convert(varchar(3), pen_anf.Gols) + ') ' + convert(varchar(3), isnull(a.gols,0))
+                                        else convert(varchar(3), isnull(a.gols,0))
+                                   end )           as Gols_Anfitriao
+			, vis.ID_Selecao                       as ID_Visitante
+			, vis.Nome_Selecao                     as Visitante
+            , convert(varchar(12), case when pen_vis.ID_jogo_selecao is not null
+                                        then convert(varchar(3), isnull(v.gols,0)) + ' (' + convert(varchar(3), pen_vis.Gols) + ')'
+                                        else convert(varchar(3), isnull(v.gols,0))
+                                   end )           as Gols_Visitante
+            , arb.Nome_Reduzido                    as Arbitro_Principal
+			, arb1.Nome_Reduzido                   as Auxiliar_1
+			, arb2.Nome_Reduzido                   as Auxiliar_2
+			, arb3.Nome_Reduzido                   as Auxiliar_3
+			, arb4.Nome_Reduzido                   as Auxiliar_4
+			, src.Observacao                       as Observacao
+			, src.ID_campeonato_edicao             as ID_Campeonato_Edicao
+			, ce.Ano                               as Ano
+			, c.Descricao                          as Campeonato            
+			, isnull(a.gols, 0)                    as GA  --Gols Anfitriao            
+			, isnull(v.gols, 0)                    as GV  --Gols Visitante    
+	     from tb_jogos_selecoes          src with(nolock)
+		 join tb_campeonatos_edicoes      ce with(nolock)on ce.ID_Campeonato_Edicao = src.ID_campeonato_edicao
+		 join tb_campeonatos               c with(nolock)on c.ID_Campeonato = ce.ID_Campeonato
+		 join tb_paises                    p with(nolock)on p.ID_Pais = ce.Pais_Sede
+		 join tb_estadios                est with(nolock)on est.ID_estadio = src.ID_estadio
+         join tb_cidades                cest with(nolock)on cest.ID_Cidade = est.ID_Cidade
+		 join tb_selecoes                anf with(nolock)on anf.ID_Selecao = src.ID_selecao_anfitriao
+    left join (
+		     select ID_jogo_selecao 
+			      , count(ID_tipo_evento) as gols
+				  , ID_selecao
+		       from tb_jogos_selecoes_eventos  with(nolock)
+              where ID_tipo_evento in (1, 4, 5, 8, 9) -- Gol, Gol Olímpico, Gol (P), Gol Contra
+              group by ID_jogo_selecao, ID_selecao
+		                                 ) a             on a.ID_jogo_selecao = src.ID_jogo_selecao
+                                                        and a.ID_selecao = src.ID_selecao_anfitriao
+		 join tb_selecoes                vis with(nolock)on vis.ID_Selecao = src.ID_selecao_visitante
+    left join (
+		     select ID_jogo_selecao 
+			      , count(ID_tipo_evento) as gols
+				  , ID_selecao
+		       from tb_jogos_selecoes_eventos  with(nolock)
+              where ID_tipo_evento in (1, 4, 5, 8, 9) -- Gol, Gol Olímpico, Gol (P), Gol Contra
+              group by ID_jogo_selecao, ID_selecao
+		                                 ) v             on v.ID_jogo_selecao = src.ID_jogo_selecao
+                                             and v.ID_selecao = src.ID_selecao_visitante		
+    left join (
+           select ID_Jogo_Selecao
+          , ID_Selecao
+                , sum(convert(int, Gol)) as Gols
+             from tb_jogos_selecoes_penaltis with(nolock)
+            group by ID_Jogo_Selecao, ID_Selecao
+                                   ) pen_anf             on pen_anf.ID_Jogo_Selecao = src.ID_Jogo_Selecao
+                                                        and pen_anf.ID_Selecao = src.ID_Selecao_Anfitriao
+    left join (
+           select ID_Jogo_Selecao
+                , ID_Selecao
+                , sum(convert(int, Gol)) as Gols
+             from tb_jogos_selecoes_penaltis with(nolock)
+            group by ID_Jogo_Selecao
+                   , ID_Selecao
+                                   ) pen_vis             on pen_vis.ID_Jogo_Selecao = src.ID_Jogo_Selecao
+                                                        and pen_vis.ID_Selecao = src.ID_Selecao_Visitante
+	     join vw_arbitros			     arb with(nolock)on arb.ID_Arbitro = src.ID_arbitro
+	left join vw_arbitros               arb1 with(nolock)on arb1.ID_Arbitro = src.ID_arbitro_aux_1
+	left join vw_arbitros               arb2 with(nolock)on arb2.ID_Arbitro = src.ID_arbitro_aux_2
+	left join vw_arbitros               arb3 with(nolock)on arb3.ID_Arbitro = src.ID_arbitro_aux_3
+	left join vw_arbitros               arb4 with(nolock)on arb4.ID_Arbitro = src.ID_arbitro_aux_4 
+        group by src.ID_jogo_selecao 
+               , format(src.Data_Jogo, 'dd/MM/yyyy')
+               , concat(ce.Ano, ' ', c.Descricao, ' - ', p.Nome_Pais )
+			   , est.Nome_Reduzido
+               , cest.Nome_Cidade  
+			   , src.ID_Campeonato_Fase
+               , dbo.fn_nome_fase(src.ID_Jogo_Selecao)
+			   , anf.ID_Selecao
+			   , anf.Nome_Selecao
+               , convert(varchar(12), case when pen_anf.ID_jogo_selecao is not null                                          
+                                           then '(' + convert(varchar(3), pen_anf.Gols) + ') ' + convert(varchar(3), isnull(a.gols,0))
+                                           else convert(varchar(3), isnull(a.gols,0))
+                                      end )
+               , vis.ID_Selecao
+			   , vis.Nome_Selecao
+               , convert(varchar(12), case when pen_vis.ID_jogo_selecao is not null
+                                           then convert(varchar(3), isnull(v.gols,0)) + ' (' + convert(varchar(3), pen_vis.Gols) + ')'
+                                           else convert(varchar(3), isnull(v.gols,0))
+                                      end )
+               , arb.Nome_Reduzido
+			   , arb1.Nome_Reduzido
+			   , arb2.Nome_Reduzido
+			   , arb3.Nome_Reduzido
+			   , arb4.Nome_Reduzido
+			   , src.Observacao
+			   , src.ID_campeonato_edicao     
+			   , ce.Ano
+			   , c.Descricao
+			   , p.Nome_Pais
+               , src.Espectadores
+               , isnull(a.gols, 0)
+               , isnull(v.gols, 0)
+
