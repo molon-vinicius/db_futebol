441ff1a6d336c516b25a894bdfc6b9d4b5314efe
Vinicius Molon <molon.vinicius@gmail.com>
2025-10-05 12:03:20 -0300

    Update tr_selecoes_tecnicos.sql

--- create_trigger/tr_selecoes_tecnicos.sql
+++ create_trigger/tr_selecoes_tecnicos.sql
@@ -1,65 +1,35 @@
-create trigger tr_selecoes_tecnicos
-            on tb_selecoes_tecnicos
-for insert, update 
+CREATE OR ALTER TRIGGER tr_selecoes_tecnicos
+ON dbo.tb_selecoes_tecnicos
+FOR INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON
 
-as
+    IF EXISTS (
+        SELECT 1
+          FROM inserted i
+     LEFT JOIN tb_campeonatos_edicoes_selecoes_part   p  ON p.ID_Campeonato_Edicao = i.ID_Campeonato_Edicao
+                                                        AND p.ID_Selecao = i.ID_Selecao
+         WHERE p.ID_Selecao IS NULL
+    )
+    BEGIN
+        RAISERROR ('Seleção não participou desta edição da competição.', 11, 127)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
 
-begin
-declare @id_sel     int
-declare @id_camp_ed int
-declare @id_tec     int
-
-        select @id_camp_ed  = ID_Campeonato_Edicao 
-             , @id_sel      = ID_Selecao
-             , @id_tec      = ID_Tecnico
-          from inserted
-  
-  if (select ID_Selecao_Tecnico from deleted) is not null
-  begin
-    if not exists (
-           select ID_Selecao 
-             from tb_campeonatos_edicoes_selecoes_part with(nolock)
-            where ID_Campeonato_Edicao = @id_camp_ed
-              and ID_Selecao = @id_sel
-    )              
-    begin
-       raiserror ('Seleção não participou desta edição da competição.', 11, 127)
-       rollback transaction
-    end
-
-    if (select count(ID_Selecao_Tecnico) as qtd 
-          from tb_selecoes_tecnicos with(nolock)
-         where ID_Campeonato_Edicao = @id_camp_ed
-           and ID_Tecnico = @id_tec) > 1
-    begin
-       raiserror ('Técnico já cadastrado em outra seleção.', 11, 127)
-       rollback transaction
-    end
-  end
-  
-  if (select ID_Selecao_Tecnico from deleted) is null
-  begin
-    if not exists (
-           select ID_Selecao 
-             from tb_campeonatos_edicoes_selecoes_part with(nolock)
-            where ID_Campeonato_Edicao = @id_camp_ed
-              and ID_Selecao = @id_sel
-    )              
-    begin
-       raiserror ('Seleção não participou desta edição da competição.', 11, 127)
-       rollback transaction
-    end
-
-    if (select count(ID_Selecao_Tecnico) as qtd 
-          from tb_selecoes_tecnicos with(nolock)
-         where ID_Campeonato_Edicao = @id_camp_ed
-           and ID_Tecnico = @id_tec) > 1
-    begin
-       raiserror ('Técnico já cadastrado em outra seleção.', 11, 127)
-       rollback transaction
-    end
-
-  end
-  
-end
-
+    IF EXISTS (
+        SELECT i.ID_Tecnico, i.ID_Campeonato_Edicao
+          FROM inserted i
+          JOIN tb_selecoes_tecnicos t   ON t.ID_Campeonato_Edicao = i.ID_Campeonato_Edicao
+                                       AND t.ID_Tecnico = i.ID_Tecnico
+                                       AND t.ID_Selecao_Tecnico <> i.ID_Selecao_Tecnico
+         GROUP BY i.ID_Tecnico, i.ID_Campeonato_Edicao
+    )
+    BEGIN
+        RAISERROR ('Técnico já cadastrado em outra seleção desta edição.', 11, 127)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
+END
+GO
