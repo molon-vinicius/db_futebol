de5c0b73903ec2856a53895f15f1cd5f3c4ce173
Vinicius Molon <molon.vinicius@gmail.com>
2025-10-05 11:54:11 -0300

    Update tr_sel_elencos_cap.sql

--- create_trigger/tr_sel_elencos_cap.sql
+++ create_trigger/tr_sel_elencos_cap.sql
@@ -1,152 +1,75 @@
-create trigger tr_sel_elencos_cap
-            on tb_selecoes_elencos_capitaes
-for insert, update 
-  
-as
+CREATE OR ALTER TRIGGER tr_sel_elencos_cap
+                     ON tb_selecoes_elencos_capitaes
+FOR INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON
 
-begin
+    IF EXISTS (
+        SELECT 1
+          FROM inserted i
+          JOIN deleted  d ON i.ID_Jogo_Selecao <> d.ID_Jogo_Selecao
+    )
+    BEGIN
+        RAISERROR ('Não é possível alterar o ID do jogo, necessário excluir e inserir novamente as informações.', 11, 127)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
 
-declare @id_jogo_sel int
-declare @id_sel      int
-declare @id_jgd      int
-declare @old_id_jogo_sel int
-declare @old_id_sel      int
-declare @old_id_jgd      int
-declare @erro varchar(1000)
+    IF EXISTS (
+        SELECT 1
+          FROM inserted i
+     LEFT JOIN deleted  d ON 1=1
+         WHERE NOT EXISTS (
+                 SELECT 1
+                   FROM tb_jogos_selecoes js WITH (NOLOCK)
+                  WHERE js.ID_Jogo_Selecao = i.ID_Jogo_Selecao
+                    AND i.ID_Selecao IN (js.ID_Selecao_Anfitriao, js.ID_Selecao_Visitante)
+        )
+    )
+    BEGIN
+        RAISERROR ('Não é possível inserir o capitão, pois a seleção informada não faz parte dessa partida.', 11, 127)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
 
-      select @id_jogo_sel = ID_Jogo_Selecao
-           , @id_sel      = ID_Selecao
-           , @id_jgd      = ID_Jogador
-        from inserted
-  
-  if (select ID_Jogo_Selecao from deleted) is not null 
-  begin
-      select @old_id_jogo_sel = ID_Jogo_Selecao
-           , @old_id_sel      = ID_Selecao
-           , @old_id_jgd      = ID_Jogador
-        from deleted   
+    IF EXISTS (
+        SELECT 1
+          FROM inserted i
+         WHERE NOT EXISTS (
+               SELECT 1
+                 FROM tb_jogos_selecoes js WITH (NOLOCK)
+            LEFT JOIN tb_jogos_selecoes_anfitrioes ja WITH (NOLOCK) ON ja.ID_Jogo_Selecao = js.ID_Jogo_Selecao
+            LEFT JOIN tb_jogos_selecoes_visitantes jv WITH (NOLOCK) ON jv.ID_Jogo_Selecao = js.ID_Jogo_Selecao
+                WHERE js.ID_Jogo_Selecao = i.ID_Jogo_Selecao
+                  AND i.ID_Jogador IN (ja.ID_Jogador, jv.ID_Jogador)
+        )
+    )
+    BEGIN
+        RAISERROR ('Jogador informado não faz parte de nenhuma das seleções da partida.', 11, 127)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
 
-    if @old_id_jogo_sel <> @id_jogo_sel
-    begin
-      raiserror ('Não é possível alterar o ID do jogo, necessário excluir e inserir novamente as informações.', 11, 127)
-      rollback transaction
-    end
-
-	if @old_id_sel <> @id_sel
-    begin
-       if not exists (
-          select ID_selecao_anfitriao
-            from tb_jogos_selecoes with(nolock)
-           where ID_jogo_selecao = @id_jogo_sel
-             and ID_selecao_anfitriao = @id_sel
-
-           union all
-
-          select ID_selecao_visitante
-            from tb_jogos_selecoes with(nolock)
-           where ID_jogo_selecao = @id_jogo_sel
-             and ID_selecao_visitante = @id_sel
-	   )
-	   begin
-          raiserror ('Não é possível inserir o capitão, pois a seleção informada não faz parte dessa partida.', 11, 127)
-          rollback transaction
-       end
-    end
-
-	if @old_id_jgd <> @id_jgd
-	begin
-       if not exists (
-          select ID_Jogador
-            from tb_jogos_selecoes            a with(nolock)
-            join tb_jogos_selecoes_anfitrioes b with(nolock)on b.ID_jogo_selecao = a.ID_jogo_selecao
-           where b.ID_jogo_selecao = @id_jogo_sel
-             and b.ID_Jogador = @id_jgd
-
-           union all
-
-          select ID_Jogador
-            from tb_jogos_selecoes            a with(nolock)
-            join tb_jogos_selecoes_visitantes b with(nolock)on b.ID_jogo_selecao = a.ID_jogo_selecao
-           where b.ID_jogo_selecao = @id_jogo_sel
-             and b.ID_Jogador = @id_jgd
-   	   )
-       begin
-          raiserror ('Jogador informado não faz parte de nenhuma das seleções da partida.', 11, 127)
-          rollback transaction
-       end
-    end
-
-  end
-
-  if (select ID_Jogo_Selecao from deleted) is null
-  begin
-    if not exists (
-       select ID_selecao_anfitriao
-         from tb_jogos_selecoes with(nolock)
-        where ID_jogo_selecao = @id_jogo_sel
-          and ID_selecao_anfitriao = @id_sel
-
-        union all
-
-       select ID_selecao_visitante
-         from tb_jogos_selecoes with(nolock)
-        where ID_jogo_selecao = @id_jogo_sel
-          and ID_selecao_visitante = @id_sel
-	)
-    begin
-       raiserror ('Não é possível inserir o capitão, pois a seleção informada não faz parte dessa partida.', 11, 127)
-       rollback transaction
-    end
-
-    if not exists (
-       select ID_Jogador
-         from tb_jogos_selecoes            a with(nolock)
-         join tb_jogos_selecoes_anfitrioes b with(nolock)on b.ID_jogo_selecao = a.ID_jogo_selecao
-        where b.ID_jogo_selecao = @id_jogo_sel
-          and b.ID_Jogador = @id_jgd
-
-        union all
-
-       select ID_Jogador
-         from tb_jogos_selecoes            a with(nolock)
-         join tb_jogos_selecoes_visitantes b with(nolock)on b.ID_jogo_selecao = a.ID_jogo_selecao
-        where b.ID_jogo_selecao = @id_jogo_sel
-          and b.ID_Jogador = @id_jgd
-	)
-    begin
-       raiserror ('Jogador informado não faz parte de nenhuma das seleções da partida.', 11, 127)
-       rollback transaction
-    end
-
-    if exists (
-       select 1
-         from tb_jogos_selecoes            a with(nolock)
-         join tb_jogos_selecoes_anfitrioes b with(nolock)on b.ID_Jogo_Selecao = a.ID_Jogo_Selecao
-         join tb_selecoes_elencos          c with(nolock)on c.ID_Selecao = b.ID_Selecao
-                                                        and c.ID_jogador = b.ID_Jogador
-                                                        and c.ID_Campeonato_Edicao = a.ID_Campeonato_Edicao
-        where a.ID_Jogo_Selecao = @id_jogo_sel
-          and b.ID_selecao = @id_sel
-          and c.Capitao in ('P', 'S')
-
-        union all
-
-       select 1
-         from tb_jogos_selecoes            a with(nolock)
-         join tb_jogos_selecoes_visitantes b with(nolock)on b.ID_Jogo_Selecao = a.ID_Jogo_Selecao
-         join tb_selecoes_elencos          c with(nolock)on c.ID_Selecao = b.ID_Selecao
-                                                        and c.ID_jogador = b.ID_Jogador
-                                                        and c.ID_Campeonato_Edicao = a.ID_Campeonato_Edicao
-        where a.ID_Jogo_Selecao = @id_jogo_sel
-          and b.ID_selecao = @id_sel
-          and c.Capitao in ('P', 'S')
-    ) 
-    begin
-       raiserror ('Seleção já tem um capitão definido para essa partida.', 11, 127)
-       rollback transaction
-    end
-
-  end
-
-end
-
+    IF EXISTS (
+        SELECT 1
+          FROM inserted i
+          JOIN tb_jogos_selecoes  js WITH (NOLOCK)ON js.ID_Jogo_Selecao = i.ID_Jogo_Selecao
+          JOIN tb_selecoes_elencos c WITH (NOLOCK)ON c.ID_Selecao = i.ID_Selecao
+                                                 AND c.ID_Campeonato_Edicao = js.ID_Campeonato_Edicao
+         WHERE c.Capitao IN ('P', 'S')
+           AND EXISTS (
+                   SELECT 1
+                     FROM tb_selecoes_elencos_capitaes x
+                    WHERE x.ID_Jogo_Selecao = i.ID_Jogo_Selecao
+                      AND x.ID_Selecao = i.ID_Selecao
+                      AND x.ID_Jogador <> i.ID_Jogador
+          )
+    )
+    BEGIN
+        RAISERROR ('Seleção já tem um capitão definido para essa partida.', 11, 127)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
+END
+GO
